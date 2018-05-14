909138cd7ec951b04907deaf6f4488d36871e05c
Brian Stolz <brian@tecnobrat.com>
2018-05-14 14:09:32 -0700

    Add test

--- test/serve.js
+++ test/serve.js
@@ -18,6 +18,10 @@ describe('serve', () => {
     agent.get('/pub/static-file.txt').expect(200, 'testing testing' + EOL)
   )
 
+  it('responds with found for sub paths', () =>
+    agent.get('/pub/sub/static-file.txt').expect(200, 'testing testing sub' + EOL)
+  )
+
   it('404 Not Founds for missing static files', () =>
     agent.get('/pub/not-a-file.png').expect(404)
   )
