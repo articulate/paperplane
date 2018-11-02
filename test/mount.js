5cba101f0471f0a3b6e8bb7127df322ac0dd9ab5
Scott McCormack <scott.mccormack@articulate.com>
2018-11-01 21:10:33 -0400

    More tests and lambda adjustments

--- test/mount.js
+++ test/mount.js
@@ -7,7 +7,6 @@ const Boom         = require('boom')
 const future       = require('redux-future').default
 const http         = require('http')
 const Joi          = require('joi')
-// const property     = require('prop-factory')
 const request      = require('supertest')
 const spy          = require('@articulate/spy')
 const str          = require('string-to-stream')
@@ -27,7 +26,7 @@ describe('mount', () => {
     '/json':     K(json({})),
     '/protocol': compose(json, pick(['protocol'])),
     '/none':     K({ body: undefined }),
-    '/stream':   K({ body: str('stream') }),
+    '/stream':   () => ({ body: str('stream') }),
     '/string':   K({ body: 'string' }),
     '/url':      compose(json, pick(['pathname', 'query']))
   })
@@ -248,81 +247,151 @@ describe('mount', () => {
         )
       )
 
-    //   it('parses the protocol', () =>
-    //     agent.get('/protocol')
-    //       .expect(200, { protocol: 'http' })
-    //   )
+      it('parses the protocol', () =>
+        handler({
+          httpMethod: 'GET',
+          path: '/protocol'
+        }).then(res =>
+          expect(res).to.include({
+            body: '{"protocol":"http"}',
+            statusCode: 200
+          })
+        )
+      )
 
-    //   it('parses the cookies', function(done) {
-    //     agent.get('/cookie')
-    //       .set('cookie', 'foo=bar; equation=E%3Dmc%5E2')
-    //       .end((err, res) => {
-    //         expect(res.body.foo).to.equal('bar')
-    //         expect(res.body.equation).to.equal('E=mc^2')
-    //         done()
-    //       })
-    //   })
+      it('parses the cookies', () =>
+        handler({
+          httpMethod: 'GET',
+          path: '/cookie',
+          headers: { cookie: 'foo=bar; equation=E%3Dmc%5E2' }
+        }).then(res =>
+          expect(res).to.include({
+            body: '{"foo":"bar","equation":"E=mc^2"}',
+            statusCode: 200
+          })
+        )
+      )
     })
 
-    // describe('request headers', () => {
-    //   describe('if-none-match header does not match etag', () => {
-    //     it('returns 200 with full response body', () =>
-    //       agent.get('/string').set({ 'if-none-match': '"not-the-right-etag"' })
-    //         .expect(200)
-    //         .expect('etag', '"6-tFz/4ITdPSDZKL7oXnsPIQ"')
-    //         .then(assertBody('string'))
-    //     )
-    //   })
+    describe('request headers', () => {
+      describe('if-none-match header does not match etag', () => {
+        it('returns 200 with full response body', () =>
+          handler({
+            httpMethod: 'GET',
+            path: '/string',
+            headers: { 'if-none-match': '"not-the-right-etag"' }
+          }).then(res => {
+            expect(res).to.include({
+              body: 'string',
+              statusCode: 200
+            })
+            expect(res.headers).to.include({
+              etag: '"6-tFz/4ITdPSDZKL7oXnsPIQ"'
+            })
+          })
+        )
+      })
 
-    //   describe('if-none-match header matches etag', () => {
-    //     it('returns 304 with empty response body', () =>
-    //       agent.get('/string').set({ 'if-none-match': '"6-tFz/4ITdPSDZKL7oXnsPIQ"' })
-    //         .expect(304, '')
-    //         .expect('etag', '"6-tFz/4ITdPSDZKL7oXnsPIQ"')
-    //     )
-    //   })
+      describe('if-none-match header matches etag', () => {
+        it('returns 304 with empty response body', () =>
+          handler({
+            httpMethod: 'GET',
+            path: '/string',
+            headers: { 'if-none-match': '"6-tFz/4ITdPSDZKL7oXnsPIQ"' }
+          }).then(res => {
+            expect(res.body).to.be.undefined
+            expect(res.headers).to.include({
+              etag: '"6-tFz/4ITdPSDZKL7oXnsPIQ"'
+            })
+            expect(res.statusCode).to.equal(304)
+          })
+        )
+      })
 
-    //   describe('when content-length is "0"', () => {
-    //     it('does not parse the body', () =>
-    //       agent.get('/body')
-    //         .set('content-length', '0')
-    //         .expect(200)
-    //     )
-    //   })
+      describe('when content-length is "0"', () => {
+        it('does not parse the body', () =>
+          handler({
+            httpMethod: 'GET',
+            path: '/body',
+            headers: { 'content-length': '0' }
+          }).then(res =>
+            expect(res.statusCode).to.equal(200)
+          )
+        )
+      })
 
-    //   describe('when content-type is missing', () => {
-    //     it('does not explode', () =>
-    //       agent.post('/body')
-    //         .send('body')
-    //         .set('content-type', '')
-    //         .expect(200)
-    //     )
-    //   })
+      describe('when content-type is missing', () => {
+        it('does not explode', () =>
+          handler({
+            httpMethod: 'POST',
+            path: '/body',
+            headers: { 'content-type': '' },
+            body: 'body'
+          }).then(res =>
+            expect(res.statusCode).to.equal(200)
+          )
+        )
+      })
 
-    //   it('uses x-forwarded-proto when present', () =>
-    //     agent.get('/protocol')
-    //       .set('x-forwarded-proto', 'https')
-    //       .expect(200, { protocol: 'https' })
-    //   )
-    // })
+      it('uses x-forwarded-proto when present', () =>
+        handler({
+          httpMethod: 'GET',
+          path: '/protocol',
+          headers: { 'x-forwarded-proto': 'https' }
+        }).then(res =>
+          expect(res).to.include({
+            body: '{"protocol":"https"}',
+            statusCode: 200
+          })
+        )
+      )
+    })
 
-    // describe('response body', () => {
-    //   it('accepts a buffer', () =>
-    //     agent.get('/buffer').expect(200).then(assertBody('buffer'))
-    //   )
+    describe('response body', () => {
+      it('accepts a buffer', () =>
+        handler({
+          httpMethod: 'GET',
+          path: '/buffer'
+        }).then(res =>
+          expect(res).to.include({
+            body: 'YnVmZmVy',
+            isBase64Encoded: true,
+            statusCode: 200
+          })
+        )
+      )
 
-    //   it('accepts a string', () =>
-    //     agent.get('/string').expect(200).then(assertBody('string'))
-    //   )
+      it('accepts a string', () =>
+        handler({
+          httpMethod: 'GET',
+          path: '/string'
+        }).then(res =>
+          expect(res).to.include({
+            body: 'string',
+            isBase64Encoded: false,
+            statusCode: 200
+          })
+        )
+      )
 
-    //   it('accepts a stream', () =>
-    //     agent.get('/stream').expect(200, 'stream')
-    //   )
+      it('accepts a stream', () =>
+        // agent.get('/stream').expect(200, 'stream')
+        handler({
+          httpMethod: 'GET',
+          path: '/stream'
+        }).then(res =>
+          expect(res).to.include({
+            body: 'c3RyZWFt',
+            isBase64Encoded: true,
+            statusCode: 200
+          })
+        )
+      )
 
     //   it('accepts undefined to denote a no-content body', () =>
     //     agent.get('/none').expect(200, '')
     //   )
-    // })
+    })
 
     // describe('response headers', () => {
     //   it('accepts an object of headers', () =>
