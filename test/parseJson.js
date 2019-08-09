5002f98511dd5824b05fe1ff9150698eeb526355
Scott McCormack <scott.mccormack@articulate.com>
2019-08-09 16:35:32 -0400

    Make sure parseJson still works in serverless mode

--- test/parseJson.js
+++ test/parseJson.js
@@ -1,3 +1,4 @@
+const { expect } = require('chai')
 const http = require('http')
 const { composeP, is, objOf, pipe, prop, tap } = require('ramda')
 const { Readable } = require('stream')
@@ -29,29 +30,83 @@ describe('parseJson', () => {
   const cry =
     tap(console.error)
 
-  const server = http.createServer(mount({ app, cry }))
-  const agent = request.agent(server)
-
   const body = '{"name":"paperplane"}'
 
-  it('parses when content-type is "application/json"', () =>
-    agent.post('/json')
-      .type('application/json')
-      .send(body)
-      .expect(200, { pkgName: 'paperplane' })
-  )
+  describe('with { lambda: false } (default)', () => {
+    const server = http.createServer(mount({ app, cry }))
+    const agent = request.agent(server)
 
-  it('parses when json content-type includes a charset', () =>
-    agent.post('/json')
-      .type('application/json; charset=utf-8')
-      .send(body)
-      .expect(200, { pkgName: 'paperplane' })
-  )
+    it('parses when content-type is "application/json"', () =>
+      agent.post('/json')
+        .type('application/json')
+        .send(body)
+        .expect(200, { pkgName: 'paperplane' })
+    )
 
-  it('does not parse when content-type is not json', () =>
-    agent.post('/plain')
-      .type('text/plain')
-      .send('just plain text')
-      .expect(200, { isReadable: true })
-  )
+    it('parses when json content-type includes a charset', () =>
+      agent.post('/json')
+        .type('application/json; charset=utf-8')
+        .send(body)
+        .expect(200, { pkgName: 'paperplane' })
+    )
+
+    it('does not parse when content-type is not json', () =>
+      agent.post('/plain')
+        .type('text/plain')
+        .send('just plain text')
+        .expect(200, { isReadable: true })
+    )
+  })
+
+  describe('with { lambda: true }', () => {
+    const handler = mount({ app, cry, lambda: true })
+
+    it('parses when content-type is "application/json"', () =>
+      handler({
+        httpMethod: 'POST',
+        path: '/json',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body
+      }).then(res =>
+        expect(res).to.include({
+          body: '{"pkgName":"paperplane"}',
+          statusCode: 200
+        })
+      )
+    )
+
+    it('parses when json content-type includes a charset', () =>
+      handler({
+        httpMethod: 'POST',
+        path: '/json',
+        headers: {
+          'content-type': 'application/json; charset=utf-8'
+        },
+        body
+      }).then(res =>
+        expect(res).to.include({
+          body: '{"pkgName":"paperplane"}',
+          statusCode: 200
+        })
+      )
+    )
+
+    it('does not parse when content-type is not json', () =>
+      handler({
+        httpMethod: 'POST',
+        path: '/plain',
+        headers: {
+          'content-type': 'text/plain'
+        },
+        body
+      }).then(res =>
+        expect(res).to.include({
+          body: '{"isReadable":false}',
+          statusCode: 200
+        })
+      )
+    )
+  })
 })
